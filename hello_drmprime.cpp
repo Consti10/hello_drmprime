64c41bdf81592225230f8ae08791764aa4329da6
consti10 <geierconstantinabc@gmail.com>
2022-01-29 03:20:17 +0100

    add delay debugging

--- hello_drmprime.cpp
+++ hello_drmprime.cpp
@@ -119,7 +119,7 @@ static int decode_write(AVCodecContext * const avctx,
         if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
             av_frame_free(&frame);
             av_frame_free(&sw_frame);
-            fprintf(stderr, "Got eagain\n");
+            //fprintf(stderr, "Got eagain\n");
             // Consti10
             // return 0;
             continue;
