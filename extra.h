b49a82f082aab9e9adeab25aeff495a971fd3cee
consti10 <geierconstantinabc@gmail.com>
2022-02-03 16:22:52 +0100

    add basic code for latency testing

--- extra.h
+++ extra.h
@@ -57,6 +57,8 @@ void check_single_nalu(const uint8_t* data,const size_t data_length){
                             cb(nalu);
                         }
                     }*/
+                    const size_t naluLen=nalu_data_position-4;
+                    std::cout<<"Found nalu of len:"<<naluLen<<"\n";
                     nNALUs++;
                     nalu_data_position = 4;
                 }
