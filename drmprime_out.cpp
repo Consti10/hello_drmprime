454e54284823ddcb8e790e2e601c44e1b34b7170
consti10 <geierconstantinabc@gmail.com>
2022-02-20 14:31:57 +0100

    testing

--- drmprime_out.cpp
+++ drmprime_out.cpp
@@ -322,7 +322,7 @@ static int do_display(drmprime_out_env_t *const de, AVFrame *frame)
         da_init(de,da,frame);
         first=false;
     }else{
-        av_hwframe_map(da->frame,frame);
+        av_hwframe_map(da->frame,frame,0);
     }
     // Not needed / doesn't have the desired effect anyways
     //waitForVSYNC(de);
