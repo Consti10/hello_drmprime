67ed276144751c70bd6f36f60f5fb8eaa2bbc6eb
consti10 <geierconstantinabc@gmail.com>
2022-02-28 22:58:34 +0100

    test drmModeSetCrtc

--- drmprime_out.cpp
+++ drmprime_out.cpp
@@ -269,7 +269,8 @@ static int da_init(drmprime_out_env_t *const de, drm_aux_t *da,AVFrame* frame){
         }else{
             fprintf(stderr, "drmModePageFlip success\n");
         }*/
-        MLOGD<<"de->con_id:"<<de->con_id<<" de->setup.conId"<<de->setup.conId<<"\n";
+        drmModeConnectorPtr xConnector=drmModeGetConnector(de->drm_fd,de->setup.conId);
+        MLOGD<<"de->con_id:"<<de->con_id<<" de->setup.conId"<<de->setup.conId<<" actual connector"<<xConnector<<"\n";
         uint32_t connectors[1];
         connectors[0]=de->con_id;
         if(drmModeSetCrtc(de->drm_fd,de->setup.crtcId,da->fb_handle,0,0,connectors,1,NULL)!=0){
