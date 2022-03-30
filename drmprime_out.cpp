0a30dff1b7dcfd2b0c4f7f12fbb3492c80e44e2a
consti10 <geierconstantinabc@gmail.com>
2022-03-30 22:28:22 +0200

    t6

--- drmprime_out.cpp
+++ drmprime_out.cpp
@@ -380,8 +380,8 @@ static int do_display(drmprime_out_env_t *const de, AVFrame *frame){
         MLOGD<<"YX\n";*/
         //av_frame_free(&frame);
         //av_frame_free(&frame);
-        const AVDRMObjectDescriptor *obj1 = &da->frame->data[0];
-        const AVDRMObjectDescriptor *obj2 = &frame->data[0];
+        const AVDRMObjectDescriptor *obj1 = frame->data[0].desc->objects[0];;
+        const AVDRMObjectDescriptor *obj2 = frame->data[0].desc->objects[0];;
 
         if(obj1->fd==obj2->fd){
             fprintf(stderr, "weird\n");
