160519d7b278f3a9ff5c5912f2c05b0a95241b24
consti10 <geierconstantinabc@gmail.com>
2022-08-27 12:56:33 +0200

    refactor

--- play_rtp.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=/usr/local/lib/
-./hello_drmprime -i udp://127.0.0.1:5600
--- test_latency.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=/usr/local/lib/
-./hello_drmprime -i ../ffmpeg_decoder/in/rv1126.h265 | grep Avg
