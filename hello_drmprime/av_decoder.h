7cfcc62f39102779d4e0153ffa0c4d975a44377c
consti10 <geierconstantinabc@gmail.com>
2022-08-29 13:22:43 +0200

    generify av decoder

--- hello_drmprime/av_decoder.h
+++ hello_drmprime/av_decoder.h
@@ -5,6 +5,25 @@
 #ifndef HELLO_DRMPRIME_HELLO_DRMPRIME_AV_DECODER_H_
 #define HELLO_DRMPRIME_HELLO_DRMPRIME_AV_DECODER_H_
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/pixdesc.h>
+#include <libavutil/hwcontext.h>
+#include <libavutil/opt.h>
+#include <libavutil/avassert.h>
+#include <libavutil/imgutils.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+#include <libavutil/buffer.h>
+#include <libavutil/frame.h>
+//
+#include "libavutil/frame.h"
+#include "libavutil/hwcontext.h"
+#include "libavutil/hwcontext_drm.h"
+#include "libavutil/pixdesc.h"
+}
+
 class AVDecoder {
 
 };
