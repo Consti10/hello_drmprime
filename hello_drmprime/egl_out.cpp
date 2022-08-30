6471f826e4e0c3723d4ea7afef77dcf826fe79d7
consti10 <geierconstantinabc@gmail.com>
2022-08-31 01:38:55 +0200

    add egl out via decode example

--- hello_drmprime/egl_out.cpp
+++ hello_drmprime/egl_out.cpp
@@ -232,7 +232,8 @@ void EGLOut::render_once() {
   glClearColor(1.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT |GL_DEPTH_BUFFER_BIT| GL_STENCIL_BUFFER_BIT);
   // Only render the texture if we have one (aka we have gotten at least one frame from the decoder)
-  if(true){
+  // Note that otherwise, if we render via OpenGL but the texture has no backing, nothing really happens ;)
+  if(frame_texture.has_valid_image){
 	glUseProgram(shader_program);
 	glBindTexture(GL_TEXTURE_EXTERNAL_OES, frame_texture.texture);
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
