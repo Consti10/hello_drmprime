10983eb713acb32267ed2fd4d74b7722173a227e
consti10 <geierconstantinabc@gmail.com>
2022-09-01 03:04:07 +0200

    debug

--- hello_drmprime/drmprime_out.cpp
+++ hello_drmprime/drmprime_out.cpp
@@ -211,7 +211,7 @@ static int da_init(DRMPrimeOut *const de, DRMPrimeOut::drm_aux *da,AVFrame* fram
 		return -1;
 	  }
 	}else{
-	  if(drmModePageFlip(de->drm_fd,de->setup.planeId,da->fb_id,DRM_MODE_PAGE_FLIP_EVENT | DRM_MODE_PAGE_FLIP_ASYNC, nullptr)){
+	  if(drmModePageFlip(de->drm_fd,de->setup.crtcId,da->fb_id,DRM_MODE_PAGE_FLIP_EVENT | DRM_MODE_PAGE_FLIP_ASYNC, nullptr)){
 		fprintf(stderr, "drmModePageFlip failed: %s\n", ERRSTR);
 		xFirst= true;
 		return -1;
