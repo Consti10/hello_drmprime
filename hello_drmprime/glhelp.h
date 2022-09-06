c1f4a9cb6b974122882e47810df54c80bd8093ee
consti10 <geierconstantinabc@gmail.com>
2022-09-06 17:26:02 +0200

    exp on ubuntu with nvidia

--- hello_drmprime/glhelp.h
+++ hello_drmprime/glhelp.h
@@ -4,6 +4,7 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES2/gl2ext.h>
+#include <cassert>
 
 /// Some gl extensions that don't seem to be part of raspberry gl. 
 /// I think you could get these from other places like GLAD or libepoxy.
@@ -14,6 +15,7 @@ EGLImageKHR eglCreateImageKHR(EGLDisplay dpy, EGLContext ctx, EGLenum target, EG
 	static PFNEGLCREATEIMAGEKHRPROC createImageProc = 0;
 	if(!createImageProc) {
 		createImageProc = (PFNEGLCREATEIMAGEKHRPROC)eglGetProcAddress("eglCreateImageKHR");
+		assert(createImageProc);
 	}
 	return createImageProc(dpy, ctx, target, buffer, attrib_list);
 }
@@ -24,6 +26,7 @@ EGLBoolean eglDestroyImageKHR(EGLDisplay dpy, EGLImageKHR image)
 	static PFNEGLDESTROYIMAGEKHRPROC destroyImageProc = 0;
 	if(!destroyImageProc) {
 		destroyImageProc = (PFNEGLDESTROYIMAGEKHRPROC)eglGetProcAddress("eglDestroyImageKHR");
+	  	assert(destroyImageProc);
 	}
 	return destroyImageProc(dpy, image);
 }
@@ -34,6 +37,7 @@ void glDebugMessageCallbackKHR(GLDEBUGPROCKHR callback, const void *userParam)
 	static PFNGLDEBUGMESSAGECALLBACKKHRPROC debugMessageCallbackProc = 0;
 	if(!debugMessageCallbackProc) {
 		debugMessageCallbackProc = (PFNGLDEBUGMESSAGECALLBACKKHRPROC)eglGetProcAddress("glDebugMessageCallbackKHR");
+	  	assert(debugMessageCallbackProc);
 	}
 	debugMessageCallbackProc(callback, userParam);
 }
@@ -45,6 +49,7 @@ void glEGLImageTargetTexture2DOES(GLenum target, GLeglImageOES image)
 	static PFNGLEGLIMAGETARGETTEXTURE2DOESPROC imageTargetTexture2DOES = 0;
 	if(!imageTargetTexture2DOES) {
 		imageTargetTexture2DOES = (PFNGLEGLIMAGETARGETTEXTURE2DOESPROC)eglGetProcAddress("glEGLImageTargetTexture2DOES");
+		assert(imageTargetTexture2DOES);
 	}
 	imageTargetTexture2DOES(target, image);
 }
