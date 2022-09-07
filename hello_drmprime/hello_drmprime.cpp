3cfe5f996200773e3dff6a7713184a84a490e4bf
consti10 <geierconstantinabc@gmail.com>
2022-09-08 01:58:01 +0200

    exp on ubuntu with nvidia - ok finally got a (garbage) image, but an image ;)

--- hello_drmprime/hello_drmprime.cpp
+++ hello_drmprime/hello_drmprime.cpp
@@ -340,7 +340,8 @@ int main(int argc, char *argv[]){
 	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_DRM;
 	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VAAPI;
 	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
-    const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
+    //const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
+  	const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VAAPI;
 	fprintf(stdout, "Found hw device type name: [%s]\n", av_hwdevice_get_type_name(kAvhwDeviceType));
 	/*if (type == AV_HWDEVICE_TYPE_NONE) {
 		fprintf(stderr, "Device type %s is not supported.\n", hwdev);
@@ -406,12 +407,13 @@ int main(int argc, char *argv[]){
         }
 
 		//hw_pix_fmt = AV_PIX_FMT_DRM_PRIME;
-	  	wanted_hw_pix_fmt = AV_PIX_FMT_CUDA;
+	  	//wanted_hw_pix_fmt = AV_PIX_FMT_CUDA;
 		//wanted_hw_pix_fmt = (AVPixelFormat)119;
 		//wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
 		//const int argh=(int)wanted_hw_pix_fmt;
 		//std::cout<<"ARGH:"<<av_get_pix_fmt_name(AV_PIX_FMT_CUDA)<<"("<<argh<<")\n";
 	  	//wanted_hw_pix_fmt = AV_PIX_FMT_YUV420P;
+	  	wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
     }
 
     if (!(decoder_ctx = avcodec_alloc_context3(decoder))){
