40d6313aa62fb3a16807b7ba011d7b653a7b74d2
consti10 <geierconstantinabc@gmail.com>
2022-09-08 19:54:04 +0200

    refactor out the shader

--- hello_drmprime/hello_drmprime.cpp
+++ hello_drmprime/hello_drmprime.cpp
@@ -339,8 +339,8 @@ int main(int argc, char *argv[]){
 	//const AVHWDeviceType kAvhwDeviceType = av_hwdevice_find_type_by_name(hwdev);
 	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_DRM;
 	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VAAPI;
-	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
-  	const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VDPAU;
+	const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
+  	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VDPAU;
 	fprintf(stdout, "kAvhwDeviceType name: [%s]\n", av_hwdevice_get_type_name(kAvhwDeviceType));
 	/*if (type == AV_HWDEVICE_TYPE_NONE) {
 		fprintf(stderr, "Device type %s is not supported.\n", hwdev);
@@ -406,11 +406,11 @@ int main(int argc, char *argv[]){
         }
 
 		//wanted_hw_pix_fmt = AV_PIX_FMT_DRM_PRIME;
-	  	//wanted_hw_pix_fmt = AV_PIX_FMT_CUDA;
+	  	wanted_hw_pix_fmt = AV_PIX_FMT_CUDA;
 		//wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
 	  	//wanted_hw_pix_fmt = AV_PIX_FMT_YUV420P;
 	  	//wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
-		wanted_hw_pix_fmt = AV_PIX_FMT_VDPAU;
+		//wanted_hw_pix_fmt = AV_PIX_FMT_VDPAU;
     }
 
     if (!(decoder_ctx = avcodec_alloc_context3(decoder))){
