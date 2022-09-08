f662f59f8893235f72d82bd319f55fb92b30fb6e
consti10 <geierconstantinabc@gmail.com>
2022-09-08 20:59:47 +0200

    rename

--- hello_drmprime/hello_drmprime.cpp
+++ hello_drmprime/hello_drmprime.cpp
@@ -338,8 +338,8 @@ int main(int argc, char *argv[]){
   	print_codecs_h264_h265_mjpeg();
 	//const AVHWDeviceType kAvhwDeviceType = av_hwdevice_find_type_by_name(hwdev);
 	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_DRM;
-	const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VAAPI;
-	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
+	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VAAPI;
+	const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_CUDA;
   	//const AVHWDeviceType kAvhwDeviceType = AV_HWDEVICE_TYPE_VDPAU;
 	fprintf(stdout, "kAvhwDeviceType name: [%s]\n", av_hwdevice_get_type_name(kAvhwDeviceType));
 	/*if (type == AV_HWDEVICE_TYPE_NONE) {
@@ -406,8 +406,8 @@ int main(int argc, char *argv[]){
         }
 
 		//wanted_hw_pix_fmt = AV_PIX_FMT_DRM_PRIME;
-	  	//wanted_hw_pix_fmt = AV_PIX_FMT_CUDA;
-		wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
+	  	wanted_hw_pix_fmt = AV_PIX_FMT_CUDA;
+		//wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
 	  	//wanted_hw_pix_fmt = AV_PIX_FMT_YUV420P;
 	  	//wanted_hw_pix_fmt = AV_PIX_FMT_VAAPI;
 		//wanted_hw_pix_fmt = AV_PIX_FMT_VDPAU;
