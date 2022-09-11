edc1e93b22758c6f05430aa8a16161d2f1083dca
consti10 <geierconstantinabc@gmail.com>
2022-09-11 22:01:10 +0200

    tmp rpi

--- hello_drmprime/ffmpeg_workaround_api_version.h
+++ hello_drmprime/ffmpeg_workaround_api_version.h
@@ -6,13 +6,13 @@
 #define HELLO_DRMPRIME__FFMPEG_WORKAROUND_API_VERSION_H_
 
 // For some reaseon av_frame_cropped_width doesn't exit on ffmpeg default on ubuntu
-/*static int av_frame_cropped_width(const AVFrame* frame){
+static int av_frame_cropped_width(const AVFrame* frame){
   return frame->width;
 }
 
 static int av_frame_cropped_height(const AVFrame* frame){
   return frame->height;
-}*/
+}
 
 
 static std::string safe_av_hwdevice_get_type_name(enum AVHWDeviceType type){
@@ -20,7 +20,7 @@ static std::string safe_av_hwdevice_get_type_name(enum AVHWDeviceType type){
   if(tmp== nullptr){
 	return "null";
   }
-  return std::string(tmp);
+  return {tmp};
 }
 
 static std::string safe_av_get_pix_fmt_name(enum AVPixelFormat pix_fmt){
@@ -28,7 +28,7 @@ static std::string safe_av_get_pix_fmt_name(enum AVPixelFormat pix_fmt){
   if(tmp== nullptr){
 	return "null";
   }
-  return std::string(tmp);
+  return {tmp};
 }
 
 #endif //HELLO_DRMPRIME__FFMPEG_WORKAROUND_API_VERSION_H_
